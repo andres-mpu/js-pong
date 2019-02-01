21d6bc82c3c44f10668c0ec474fcf8b48a20d8be
Yhorm <aguilar_andres@protonmail.com>
2019-01-31 18:59:03 -0800

    updated getDistance method

--- pong.js
+++ pong.js
@@ -11,11 +11,9 @@ addEventListener("resize", function() {
   // calling init resizes ojects rather than creating new ones
   init();
 });
-
+// use pythagoran theorem to calculate distance
 function getDistance(x1, y1, x2, y2) {
-
-  return Math.sqrt( Math.pow( (x2 - x1), 2) + Math.pow( (y2 - y1), 2));
-
+  return Math.sqrt( Math.pow(x2 - x1, 2 ) + Math.pow(y2 - y1, 2) );
 }
 
 class Circle {
@@ -146,6 +144,7 @@ function animate() {
   cir.update();
   rec1.update();
   rec2.update();
+
 }
 
 init();
